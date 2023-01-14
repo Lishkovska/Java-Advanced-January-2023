4faf82c5a330e397291f14bce1b09027ad306e96
Lishkovska <114057232+Lishkovska@users.noreply.github.com>
2023-01-14 14:57:43 +0200

    add solution for PrintDiagonalsOfMatrix_

--- src/MultidimensionalArraysLesson/PrintDiagonalsOfSquareMatrix_06.java
+++ src/MultidimensionalArraysLesson/PrintDiagonalsOfSquareMatrix_06.java
@@ -1,10 +1,29 @@
 package MultidimensionalArraysLesson;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrintDiagonalsOfSquareMatrix_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    }
-    }
+        int size = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+
+            matrix[i] = arr;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(matrix[i][i] + " ");
+            }
+        System.out.println();
+
+        for (int row = size - 1 ,col = 0 ; col < size; row-- ,col++ ) {
+            System.out.print(matrix[row][col] + " ");
+          }
+        }
+     }
+    
