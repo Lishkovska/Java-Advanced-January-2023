df9a46cd8c3f2eec6aab6b8d70c2ac68d10514d6
Lishkovska <114057232+Lishkovska@users.noreply.github.com>
2023-01-14 15:24:40 +0200

    Add solution for SumDiagonalDifference

--- src/multidimensionalArraysExercise/_03_DiagonalDifference.java
+++ src/multidimensionalArraysExercise/_03_DiagonalDifference.java
@@ -1,10 +1,34 @@
 package multidimensionalArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _03_DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int size = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt).toArray();
+
+            matrix[i] = arr;
+        }
+       int firstDiagonal;
+        int sumFirstDiagonal = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            firstDiagonal = matrix[i][i];
+            sumFirstDiagonal += firstDiagonal;
+        }
+        int secondDiagonal;
+        int sumSecondDiagonal = 0;
+        for (int row = matrix.length - 1, col = 0; col < matrix.length ; row-- , col++){
+            secondDiagonal = matrix[row][col];
+            sumSecondDiagonal += secondDiagonal;
+        }
+        int difference = Math.abs(sumFirstDiagonal - sumSecondDiagonal);
+        System.out.println(difference);
     }
 }
