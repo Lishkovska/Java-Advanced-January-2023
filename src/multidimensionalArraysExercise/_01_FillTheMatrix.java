740f9b22c081255e4de72a741319cf82095a48d4
Lishkovska <114057232+Lishkovska@users.noreply.github.com>
2023-01-15 12:43:09 +0200

    Add solutions for task01 and task02

--- src/multidimensionalArraysExercise/_01_FillTheMatrix.java
+++ src/multidimensionalArraysExercise/_01_FillTheMatrix.java
@@ -6,5 +6,54 @@ public class _01_FillTheMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] inputArr = scanner.nextLine().split(", ");
+        int size = Integer.parseInt(inputArr[0]);
+        String pattern = inputArr[1];
+
+        int[][] matrix = new int[size][size];
+
+        int startNum = 1;
+
+        if (pattern.equals("A")){
+            getPatternA(size, matrix, startNum);
+            printMatrix(size, matrix);
+        } else if (pattern.equals("B")){
+            getPatternB(size, matrix, startNum);
+            printMatrix(size, matrix);
+        }
+    }
+
+    private static void printMatrix(int size, int[][] matrix) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+               System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void getPatternB(int size, int[][] matrix, int startNum) {
+        for (int col = 0; col < size; col++) {
+            if (col % 2 == 0){
+                for (int row = 0; row < size; row++) {
+                    matrix[row][col] = startNum;
+                    startNum++ ;
+                }
+            } else {
+                for (int row = size - 1; row >= 0; row--) {
+                    matrix[row][col] = startNum;
+                    startNum++;
+                }
+            }
+        }
+    }
+
+    private static void getPatternA(int size, int[][] matrix, int startNum) {
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row ++) {
+                matrix[row][col] = startNum;
+                startNum++ ;
+            }
+        }
     }
 }
--- src/multidimensionalArraysExercise/_02_MatrixOfPalindromes.java
+++ src/multidimensionalArraysExercise/_02_MatrixOfPalindromes.java
@@ -1,10 +1,37 @@
 package multidimensionalArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class _02_MatrixOfPalindromes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] inputArr = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int rows = inputArr[0];
+        int cols = inputArr[1];
+
+        String[][] palindromeMatrix = new String[rows][cols];
+
+        for (int row = 0; row < rows ; row++) {
+            for (int col = 0; col < cols; col++) {
+                char outsideLetter = (char)('a' + row);
+                char insideLetter = (char)('a' + row + col) ;
+                palindromeMatrix[row][col] = "" + outsideLetter + insideLetter + outsideLetter;
+            }
+        }
+
+        printMatrix(palindromeMatrix);
+    }
+
+    private static void printMatrix(String[][] palindromeMatrix) {
+        for (int i = 0; i < palindromeMatrix.length; i++) {
+            for (int j = 0; j < palindromeMatrix[i].length; j++) {
+                System.out.print(palindromeMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
