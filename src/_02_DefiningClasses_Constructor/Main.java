3618c174448d3c0d928e77e6c6be110dc595dd4d
Lishkovska <114057232+Lishkovska@users.noreply.github.com>
2023-02-02 10:15:03 +0200

    push  solution for Constructor task

--- src/_02_DefiningClasses_Constructor/Main.java
+++ src/_02_DefiningClasses_Constructor/Main.java
@@ -7,5 +7,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+
+            if (input.length == 1){
+                Car car = new Car(input[0]);
+                System.out.println(car);
+            } else {
+                Car car = new Car(input[0], input[1], Integer.parseInt(input[2]));
+                System.out.println(car);
+            }
+        }
     }
 }
